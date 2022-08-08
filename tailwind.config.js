b6022feb9a188c495b7afb7b4e67631185877970
CJ Avilla <cjavilla@gmail.com>
2022-08-08 13:35:57 -0400

    Add devise and landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
