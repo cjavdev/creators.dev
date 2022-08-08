3a67dc2839977b2e7892901cb43894592ee45399
CJ Avilla <cjavilla@gmail.com>
2022-08-08 15:42:59 -0400

    Remove account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_one :account
 end
