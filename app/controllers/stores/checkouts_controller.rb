5b10910f3fc2089044b36344a589c6b2299de1d5
CJ Avilla <cjavilla@gmail.com>
2022-08-10 16:01:38 -0400

    Checkout confirmation page

--- app/controllers/stores/checkouts_controller.rb
+++ app/controllers/stores/checkouts_controller.rb
@@ -2,13 +2,18 @@ module Stores
   class CheckoutsController < StoreBaseController
 
     def show
-      render plain: "Thanks"
+      @checkout_session = Stripe::Checkout::Session.retrieve({
+        id: params[:session_id],
+        expand: ['line_items.data.price.product', 'payment_intent.payment_method']
+      }, {
+        stripe_account: @store.user.account.stripe_id
+      })
     end
 
     def create
       checkout_session = Stripe::Checkout::Session.create({
         mode: 'payment',
-        success_url: store_checkout_url,
+        success_url: store_checkout_url + "?session_id={CHECKOUT_SESSION_ID}",
         cancel_url: store_root_url,
         line_items: [{
           price: params[:price],
