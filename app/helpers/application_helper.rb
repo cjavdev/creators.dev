3113a601ef67128b68479423369c586c92ad5e91
CJ Avilla <cjavilla@gmail.com>
2022-08-11 16:03:43 -0400

    gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  end
+
   def menu_items
     [{
       name: 'Dashboard',
